fb185913353d98f996452989ded3ecc1a477d366
Pupler <misha.reak45@gmail.com>
2026-03-30 13:15:19 +0200

    feat: add basic bot polling with echo handler

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 using NutriBeastBot;
+using Telegram.Bot;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<ITelegramBotClient>(
+    new TelegramBotClient(builder.Configuration["BotToken"]!)
+);
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
--- Worker.cs
+++ Worker.cs
@@ -1,23 +1,55 @@
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
 namespace NutriBeastBot;
 
-public class Worker : BackgroundService
+public class Worker(ILogger<Worker> logger, ITelegramBotClient botClient) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (_logger.IsEnabled(LogLevel.Information))
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            errorHandler: HandleErrorAsync,
+            receiverOptions: new ReceiverOptions
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
+                AllowedUpdates = [UpdateType.Message, UpdateType.CallbackQuery]
+            },
+            cancellationToken: stoppingToken
+        );
+
+        await Task.Delay(Timeout.Infinite, stoppingToken);
+    }
+
+    protected Task HandleErrorAsync(
+        ITelegramBotClient bot,
+        Exception ex,
+        HandleErrorSource source,
+        CancellationToken ct
+    )
+    {
+        logger.LogError("Telegram Error: {Error}", ex);
+        return Task.CompletedTask;
+    }
+
+    protected Task HandleUpdateAsync(
+        ITelegramBotClient bot,
+        Update update,
+        CancellationToken ct
+    )
+    {
+        if (update.Message?.Text == null)
+        {
+            return Task.CompletedTask;
         }
+
+        var text = update.Message.Text;
+        var chatId = update.Message.Chat.Id;
+
+        logger.LogInformation("Message: {Text}", text);
+        bot.SendMessage(chatId, text, cancellationToken: ct);
+
+        return Task.CompletedTask;
     }
 }
